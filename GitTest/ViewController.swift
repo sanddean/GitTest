a6e1d421462f80bcec62ef0b1db59d555aded2af
Vuong Cat VO - #504783 <vcvo@utas.edu.au>
2020-04-17 18:11:22 +1000

    This commit from Cat. Test Function added

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func testfun() {
+        //this is a test function
+    }
 
 }
 
